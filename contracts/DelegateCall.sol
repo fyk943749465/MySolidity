e2d3a6d68588345ae977ddcbc0698edf29cc6475
fyk943749465 <fyk943749465@gmail.com>
2023-10-09 11:54:49 +0800

    DelegateCall合约修改提交

--- contracts/DelegateCall.sol
+++ contracts/DelegateCall.sol
@@ -1,3 +1,43 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.2 <0.9.0;
+
+
+contract A {
+    uint private num;
+    function setNum(uint _num) public {
+        num = _num;
+    }
+
+    function getNum(uint _num) public {
+        num = _num + 1;
+    }
+
+    function getNum() public view returns (uint) {
+        return num;
+    }
+
+    /*
+     * 第一种:
+     * 在 A 智能合约种调用 B 智能合约
+     * 这里调用,需要传入B合约的地址,以及一个数字,如5
+     * 这时候,调用B合约的getNum方法,就会读到结果是7
+     */
+    function bSetNum(address _bAddress, uint _num) public {
+        B b = B(_bAddress);
+        b.setNum(_num);
+    }
+}
+
+contract B {
+
+    uint private num;
+    function setNum(uint _num) public {
+        num = _num +2;
+    }
+
+    function getNum() public view returns(uint) {
+        return num;
+    }
+
+}
