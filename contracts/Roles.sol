8b7fed2d540746d1b307bb8d7927c0945ae501d1
fyk943749465 <fyk943749465@gmail.com>
2023-10-09 21:42:37 +0800

    setRoleAdmin的理解

--- contracts/Roles.sol
+++ contracts/Roles.sol
@@ -14,6 +14,9 @@ contract Roles is ERC20, AccessControl {
     // 定义了一个挖矿的角色
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
+    // 角色的ADMIN角色
+    bytes32 public constant MINTER_ADMIN_ROLE = keccak256("MINTER_ADMIN_ROLE");
+
     constructor() ERC20("Roles", "ROLE") {
         _mint(msg.sender, 1000000000 * 10 ** decimals());
         // 默认的 admin 的角色 合约创建者
@@ -25,5 +28,15 @@ contract Roles is ERC20, AccessControl {
     // 只有 MINTER_ROLE 角色的用户,才能进行挖矿
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    /*
+     * 我们可以拿到 MINTER_ROLE 的具体值 bytes32: 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6
+     * 我们也可以拿到 MINTER_ADMIN_ROLE 的具体值  bytes32: 0x70480ee89cb38eff00b7d23da25713d52ce19c6ed428691d22c58b2f615e3d67
+     * 而 setRoleAdmin 方法, 是可以 设置 role 角色的 admin账账户的.
+     * 而我想不通这样做有什么意义,但是可以这样做
+     */
+    function setRoleAdmin(bytes32 role, bytes32 adminRole) public {
+        super._setRoleAdmin(role, adminRole);
     }
 }
